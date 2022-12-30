58b419b5c38264e19e08d5f042e74686b1b48321
Gérard Collin <gerard.collin@appliweb.net>
2022-12-30 10:21:10 +0100

    Several fixes

--- scripts/_common.sh
+++ scripts/_common.sh
@@ -8,6 +8,12 @@ java_version=17
 # Java dependencies used by the app will be installed through the Java helper
 pkg_dependencies=""
 
+# List of services to be installed
+SERVICES_LIST=(ide preview project data)
+
+# List of Mongo databases to manage
+MONGO_DB_LIST=(dontCodeProjects dontCodeDemoProjects dontCodeData)
+
 #=================================================
 # PERSONAL HELPERS
 #=================================================
