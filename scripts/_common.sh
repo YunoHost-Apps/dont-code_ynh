038aa1d8f1af0d3a909638470010a6a61476a326
Gérard Collin <gerard.collin@appliweb.net>
2023-01-09 19:07:33 +0100

    Trying again to install Mongo 6.0 bullseye in CI server

--- scripts/_common.sh
+++ scripts/_common.sh
@@ -5,7 +5,7 @@
 #=================================================
 java_version=17
 
-mongo_version=4.4
+mongo_version=6.0
 
 # Java dependencies used by the app will be installed through the Java helper
 pkg_dependencies=""
