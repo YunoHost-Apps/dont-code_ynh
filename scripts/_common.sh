51e94e271946833c6ea87dbe434f9047719c9b87
Félix Piédallu <felix@piedallu.me>
2024-01-05 16:27:40 +0100

    regroup code into common.sh

--- scripts/_common.sh
+++ scripts/_common.sh
@@ -22,13 +22,31 @@ ynh_java="/usr/lib/jvm/java-${java_version}-openjdk-amd64/bin/java"
 # PERSONAL HELPERS
 #=================================================
 append_uri() {
-  local return="";
-  if [[ $1 == */ ]]; then
-    return=$1$2;
-  else
-    return=$1/$2
-  fi
-  echo "$return";
+    local return="";
+    if [[ $1 == */ ]]; then
+        return=$1$2;
+    else
+        return=$1/$2
+    fi
+    echo "$return";
+}
+
+_install_restart_script_and_sudoers() {
+    # Enable restarting of services from ssh
+    ynh_add_config --template="restart-services.sh" --destination="$install_dir/restart-services.sh"
+    chown "$app:$app" "$install_dir/restart-services.sh"
+    chmod o-rwx,gu=rwx "$install_dir/restart-services.sh"
+
+    # Add sudoers file for this specific command
+    ynh_add_config --template="dont-code-sudoers" --destination="/etc/sudoers.d/$app-sudoers"
+    chown root:root "/etc/sudoers.d/$app-sudoers"
+    chmod o-rwx,gu=r "/etc/sudoers.d/$app-sudoers"
+}
+
+_remove_restart_script_and_sudoers() {
+    if [ -f "/etc/sudoers.d/$app-sudoers" ]; then
+        ynh_secure_remove --file="/etc/sudoers.d/$app-sudoers"
+    fi
 }
 
 #=================================================
